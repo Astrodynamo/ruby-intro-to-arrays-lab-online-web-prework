966f1d9ad56cfa6320bb36ce6b07545b4d189f2a
Amanda King <amapickell@gmail.com>
2019-05-23 15:26:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+#instantiating
+def instantiate_new_array
+  []
+end
+
+def array_with_two_elements
+  
+end
+
+#indexing
+def first_element
+  
+end
+
+def third_element
+  
+end
+
+def last_element
+  
+end
+
+#ruby methods to return values
+def first_element_with_array_methods
+  
+end
+
+def last_element_with_array_methods
+  
+end
+
+#ruby methods to get information
+def length_of_array
+  
+end
